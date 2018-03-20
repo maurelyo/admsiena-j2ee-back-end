598baf93043754448b0fee352e2d11d0d600a0aa
maurelyo <aurelyo.web@gmail.com>
2018-03-19 23:12:47 -0300

    SPRING INITIALIZR

---
+++ src/main/java/com/br/siena/SienaApplication.java
@@ -0,0 +1,12 @@
+package com.br.siena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SienaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SienaApplication.class, args);
+	}
+}
