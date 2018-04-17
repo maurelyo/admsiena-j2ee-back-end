92bac8514d3e0b791b725306ca7bc7c4a4043dbe
maurelyo <aurelyo.web@gmail.com>
2018-04-16 21:52:00 -0300

    Adicionando spring web maven dependency;

--- src/main/java/com/br/siena/SienaApplication.java
+++ src/main/java/com/br/siena/SienaApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SienaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SienaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SienaApplication.class, args);
+    }
 }
